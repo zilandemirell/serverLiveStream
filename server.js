23546a5ce3f76283741421ab1838cf006f113621
zilan demirel <zilandemirell@gmail.com>
2020-02-17 13:21:38 +0300

    port config changed

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const {NodeMediaServer} = require('node-media-server');
 
 const config = {
   rtmp:  {
-    port: process.env.PORT || 1935,
+    port: 1935,
     chunk_size: 10000,
     gop_cache: false,
     ping: 1,
     ping_timeout: 1
     },
   http: {
-    port: process.env.PORT || 3000,
+    port: 3000,
     allow_origin: '*'
   }
 };
